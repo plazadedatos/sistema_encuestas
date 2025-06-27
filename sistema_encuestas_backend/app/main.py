7f1f9ce320343d07db48ad208a837050fd1a3b11
Almedesito <almedesito@gmail.com>
2025-06-27 01:32:58 -0300

    Enable CORS

--- sistema_encuestas_backend/app/main.py
+++ sistema_encuestas_backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.auth.auth_router import router as auth_router
 from app.users.routes import router as admin_router
 app = FastAPI(
@@ -6,6 +7,16 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Configuración de CORS para permitir solicitudes desde el frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Incluir rutas
 app.include_router(auth_router)
 app.include_router(admin_router)
+
