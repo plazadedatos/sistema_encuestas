cb06dee14397e490d2a024ac66b59e2b580fee79
Iset Cabrera <almedesito@gmail.com>
2025-06-26 22:14:23 -0300

    Ajusto IDs únicos de encuestas

--- sistema_encuestas_frontend_inicial/app/panel/recompensas/page.tsx
+++ sistema_encuestas_frontend_inicial/app/panel/recompensas/page.tsx
@@ -2,6 +2,7 @@
 import TopbarInterno from "../../../components/TopbarInterno";
 import Sidebar from "../../../components/Sidebar";
 export default function RecompensasPage() {
+  
   return (
     
     <div className="flex min-h-screen bg-slate-50">
