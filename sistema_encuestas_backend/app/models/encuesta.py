4f8c0fdb53918b05a503856f8732ae97f6bea875
Almedesito <almedesito@gmail.com>
2025-06-29 17:37:14 -0400

    Fix encuesta creation with auto timestamp

--- sistema_encuestas_backend/app/models/encuesta.py
+++ sistema_encuestas_backend/app/models/encuesta.py
@@ -1,10 +1,7 @@
 # app/models/encuesta.py
-from sqlalchemy import Column, Integer, String, Text, Date, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, Date, Boolean, DateTime, func
 from sqlalchemy.orm import relationship
 from app.database import Base
-from sqlalchemy import Column, DateTime, func
-
-fecha_creacion = Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
 
 class Encuesta(Base):
     __tablename__ = "encuestas"
@@ -13,7 +10,7 @@ class Encuesta(Base):
     fecha_fin = Column(Date)
     puntos_otorga = Column(Integer)
     estado = Column(Boolean, default=True)
-    fecha_creacion = Column(Date)
+    fecha_creacion = Column(DateTime(timezone=False), server_default=func.now(), nullable=False)
     id_usuario_creador = Column(Integer)
     fecha_inicio = Column(Date)
     titulo = Column(String)
